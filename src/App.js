d7e1b781141001e4909128e829b75c31b154f716
Isaac <isaacharris@oakland.edu>
2023-11-19 20:07:41 -0500

    complete app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [uName, setUName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [num, setNumber] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="App">
+      <label htmlFor="name">Full Name:</label>
+      <input type="text" id="name"></input>
+
+      <label htmlFor="userName">Username:</label>
+      <input type="text" id="userName"></input>
+
+      <label htmlFor="email">Email:</label>
+      <input type="text" id="email"></input>
+
+      <label htmlFor="address">Address:</label>
+      <input type="text" id="address"></input>
+
+      <label htmlFor="pnum">Phone Number:</label>
+      <input type="text" id="pnum"></input>
+
+      <input
+        type="submit"
+        onClick={(event) => {
+          event.preventDefault();
+          setName(document.getElementById("name").value);
+          setUName(document.getElementById("userName").value);
+          setEmail(document.getElementById("email").value);
+          setAddress(document.getElementById("address").value);
+          setNumber(document.getElementById("pnum").value);
+          console.log("Name: " + name);
+          console.log("Username: " + uName);
+          console.log("Email: " + email);
+          console.log("Address: " + address);
+          console.log("Number: " + num);
+        }}
+      ></input>
+    </form>
   );
 }
 
